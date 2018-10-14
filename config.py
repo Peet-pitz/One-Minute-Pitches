0e70a3eaf166b2530d712436aaaad3cbfcf8db38
Peter Njoroge <njoroge.pitz@gmail.com>
2018-10-14 21:27:24 +0300

    add email configurations

--- config.py
+++ config.py
@@ -11,15 +11,15 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS=True 
 
     #  email configurations
-    # MAIL_SERVER = 'smtp.googlemail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-    # simple mde  configurations
-    # SIMPLEMDE_JS_IIFE = True
-    # SIMPLEMDE_USE_CDN = True
+    simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 
 # class ProdConfig(Config):
