d6fdeddb7f1f2b805df42ddf04705d2827c1ecb6
Peter Njoroge <njoroge.pitz@gmail.com>
2018-10-14 21:19:15 +0300

    add users database table for storing users

--- app/models.py
+++ app/models.py
@@ -1,20 +1,20 @@
 
-# from . import db
-# from werkzeug.security import generate_password_hash,check_password_hash
-# from flask_login import UserMixin
-# from . import login_manager
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-# class User(UserMixin,db.Model):
-#     __tablename__= 'users'
-#     id=db.Column(db.Integer,primary_key =True)
-#     username = db.Column(db.String(255))
-#     email=db.Column(db.String(255),unique=True,index = True)
-#     pass_hash =db.Column(db.String(255))
+class User(UserMixin,db.Model):
+    __tablename__= 'users'
+    id=db.Column(db.Integer,primary_key =True)
+    username = db.Column(db.String(255))
+    email=db.Column(db.String(255),unique=True,index = True)
+    pass_hash =db.Column(db.String(255))
 
 #     @property
 #     def password(self):
